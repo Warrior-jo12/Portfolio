879618cfd75299bd31d865a20788c7e6362aea6a
warrior-jo12 <eyutialemu@gmail.com>
2023-02-22 23:01:07 +0300

    added image , icons and update the assets index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ export {default as navBarIcon3} from './img/nav-icon3.svg'
 export {default as githubIcon } from './img/githubIcon.svg'
 export {default as telegram} from './img/telegram.svg'
 
+
 export {default as headerImg} from './img/header-img.svg'
 export {default as meter1} from './img/meter1.svg'
 export {default as meter2} from './img/meter2.svg'
@@ -13,6 +14,7 @@ export {default as meter3} from './img/meter3.svg'
 export {default as colorSharp} from './img/color-sharp.png'
 
 export {default as projImg1} from './img/project-img1.png'
+export {default as ProfileIMG} from './img/ProfileIMG.jpg'
 export {default as projImg2} from './img/project-img2.png'
 export {default as projImg3} from './img/project-img3.png'
 export {default as projImg4} from './img/project-img4.png'
@@ -26,5 +28,7 @@ export {default as react} from './img/react.svg'
 export {default as node} from './img/node.svg'
 export {default as monogdb} from './img/monogdb.svg'
 export {default as mysql} from './img/mysql.svg'
+export {default as html} from './img/html.svg'
+export {default as css} from './img/css.svg'
 
 
