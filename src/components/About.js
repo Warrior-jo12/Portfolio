79f3e34b47395b2789fe83d6a1a6e8fc670a5f82
warrior-jo12 <eyutialemu@gmail.com>
2023-02-22 23:53:11 +0300

    add google drive link to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -27,8 +27,8 @@ const About = () => {
                                 <a href="https://t.me/Warrior_ey"><img src={telegram}/></a>
                             </div>
                             <p>You can download my resume from google drive 
-                                <a href='#' style={{display:'block', textDecoration: 'none', color: 'white'}}>Download Resume</a>
-                                And you can contact me by sending me your message <a style={{display:'block', textDecoration: 'none', color: 'white'}} href="mailto:eyutialemu@gmail.com?subject=Mail from my portfolio">Send to my email</a>
+                                <a href='https://drive.google.com/drive/folders/1bjipud8hsEy6ULsAtI1ay4aAdePIVjNI?usp=share_link' style={{display:'block', textDecoration: 'none', color: 'white'}}>Download Resume</a>
+                                And you can contact me by my email <a style={{display:'block', textDecoration: 'none', color: 'white'}} href="mailto:eyutialemu@gmail.com?subject=Mail from my portfolio">Send to my email</a>
                             </p>
                             
                         </div>
