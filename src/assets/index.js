18b08ee0ff7b9fd397c665ab2629bdabc97931fb
warrior-jo12 <eyutialemu@gmail.com>
2023-02-22 08:30:59 +0300

    added some icons also remove un used icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,28 @@ export {default as logo1} from './img/E1-removebg-preview2.png'
 export {default as navBarIcon1} from './img/nav-icon1.svg'
 export {default as navBarIcon2} from './img/nav-icon2.svg'
 export {default as navBarIcon3} from './img/nav-icon3.svg'
+export {default as githubIcon } from './img/githubIcon.svg'
+export {default as telegram} from './img/telegram.svg'
+
 export {default as headerImg} from './img/header-img.svg'
 export {default as meter1} from './img/meter1.svg'
 export {default as meter2} from './img/meter2.svg'
 export {default as meter3} from './img/meter3.svg'
 export {default as colorSharp} from './img/color-sharp.png'
+
 export {default as projImg1} from './img/project-img1.png'
 export {default as projImg2} from './img/project-img2.png'
 export {default as projImg3} from './img/project-img3.png'
+export {default as projImg4} from './img/project-img4.png'
+export {default as chatgpt} from './img/chatgpt.png'
+export {default as AIPROJ } from './img/AIPROJ.png'
+
+export {default as python} from './img/python.svg'
+export {default as django2} from './img/django2.svg'
+export {default as javascript_1} from './img/javascript_1.svg'
+export {default as react} from './img/react.svg'
+export {default as node} from './img/node.svg'
+export {default as monogdb} from './img/monogdb.svg'
+export {default as mysql} from './img/mysql.svg'
+
+
